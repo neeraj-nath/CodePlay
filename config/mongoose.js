447ca1fdcee79cd5a3e264a28b424a4ae112059e
neeraj nath <neeraj.nath987@gmail.com>
2023-04-01 21:52:59 +0530

    add correct mongoose connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/codeial_development');
-const db= mongoose.Connection;
+const db= mongoose.connection;
 
 db.on('error',console.error.bind(console,"Error Connecting to Database"));
 
